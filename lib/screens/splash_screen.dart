66015b5d653b1a58ac152e8501e573c481cbef86
ijazsharif <ijazsharif34@gmail.com>
2022-01-08 02:52:34 +0500

    Feature: spalsh screen created

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:multi_vendor_shop/screens/on_boarding/on_boarding.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  static const String id = 'splash_screen';
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Navigator.of(context).pushReplacementNamed(OnBoardingScreen.id);
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Splash Screen',
+          style: Theme.of(context).textTheme.headline1!.copyWith(
+                color: Colors.black,
+              ),
+        ),
+      ),
+    );
+  }
+}
